3b3f6876b773acabeeae0d446751a05522202723
dennisx <denizponce16@gmail.com>
2020-08-10 23:51:05 -0500

    react datepicker funcionando

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import DatePicker, {registerLocale} from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import es from "date-fns/locale/es";
+registerLocale("es",es);
+class App extends React.Component {
+  state = {
+    fecha: new Date("2020","08","11")
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  onChange= fecha => {
+    this.setState({fecha: fecha});
+  }
+
+  mostrarFecha = fecha => {
+    const options = {weekday:'long', year: 'numeric', month:'long', day:'numeric'};
+    alert(fecha.toLocaleDateString('es-ES', options))
+  }
+
+  render ()
+  {
+    return (
+      <>
+        <div className="contenedor">
+          <div className="center">
+            <DatePicker selected={this.state.fecha} onChange={this.onChange} locale="es" dateFormat="dd-MMMM-yyyy"/>
+            <br></br>
+            <input type="button" value="Mostrar Fecha" className="btn btn-primary" onClick={()=>this.mostrarFecha(this.state.fecha)} />
+          </div>
+        </div>
+      </>
+    );
+  }
+  
 }
 
 export default App;
